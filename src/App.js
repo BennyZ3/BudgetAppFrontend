ff2ff57cb25f9e1de6d9ff5cef1a308ae76a0b54
BennyZ3 <bennyz3@hotmail.com>
2022-01-24 11:04:18 -0500

    added path for edit transaction

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Nav from "./Components/Nav";
 import Home from "./Pages/Home";
 import NewTransaction from "./Pages/NewTransaction";
 import TransactionDetails from "./Pages/TransactionDetails";
+import EditTransaction from "./Pages/EditTransaction";
 
 function App() {
   return (
     <div className="App">
-      {/* Nav */}
       <Nav />
-      {/* main */}
       <main>
-        {/* Routes */}
         <Routes>
-          {/* Home */}
           <Route exact path="/" element={<Home />} />
           <Route path="/new" element={<NewTransaction />} />
-          {/* /transactions */}
-          {/* /transactions/:id */}
           <Route path="/transactions/:id" element={<TransactionDetails />} />
+          <Route path="/transactions/:id/edit" element={<EditTransaction />} />
         </Routes>
       </main>
     </div>
