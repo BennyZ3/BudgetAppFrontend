5182341b141e5a684497912fa0b21980946f64cf
BennyZ3 <bennyz3@hotmail.com>
2022-01-24 11:05:11 -0500

    added extra break down of expense and earnings

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Component, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import TransactionTable from "../Components/TransactionsTable";
 
 function Home() {
@@ -16,9 +16,29 @@ function Home() {
       return a + Number(b.amount);
     }, 0);
   }
+  function earnings(transactions) {
+    return transactions
+      .filter((item) => {
+        return item.amount > 0;
+      })
+      .reduce((a, b) => {
+        return a + Number(b.amount);
+      }, 0);
+  }
+  function expenses(transactions) {
+    return transactions
+      .filter((item) => {
+        return item.amount < 0;
+      })
+      .reduce((a, b) => {
+        return a + Number(b.amount);
+      }, 0);
+  }
   return (
     <div className="Home">
       <h1>Balance: {accountBalance(transactions)}</h1>
+      <h3>Earnings: {earnings(transactions)}</h3>
+      <h3>Expenses: {expenses(transactions)}</h3>
       <TransactionTable transactions={transactions} />
     </div>
   );
